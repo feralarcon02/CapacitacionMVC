937c12a1ee19ef5167cbe72a40c559d5e2e3f13c
Alarcón Fernando <feralarcon_02@hotmail.com>
2016-08-26 19:34:48 -0300

    git commit <Primeros cambios>

---
+++ AppMvc/AppMvc/Controllers/PruebaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMvc.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View("View");
+        }
+
+        public ActionResult AccionDeOtroControl()
+        {
+            //return View("../Home/Index
+            //return RedirectToAction("Index", "Home", new {id = 1 }); //Para pasar parametros como objeto
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ AppMvc/AppMvc/Controllers/StoredController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMvc.Controllers
+{
+    public class StoredController : Controller
+    {
+        // GET: Stored
+        public String Index()
+        {
+            return "Hola desde Stored.Index()";
+        }
+
+        public String Buscar(string genero)
+        {
+            string mensaje = HttpUtility.HtmlEncode("Stored.Buscar, Genero " + genero);
+            return mensaje;
+        }
+
+        public String Detalles(string id)
+        {
+            string mensaje = "Srored.Detalles, ID = " + id;
+            return mensaje;
+        }
+    }
+}
