fb7233bc01c9ceef31de18b3c366d1a5655a7c5a
Alarcón Fernando <feralarcon_02@hotmail.com>
2016-09-09 16:03:42 -0300

    Clase 4

---
+++ MusicStorePorMi/MusicStorePorMi/Controllers/PersonaController.cs
@@ -0,0 +1,37 @@
+﻿using MusicStorePorMi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStorePorMi.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Post(Persona model) //Recibe directamente el modelo
+        {
+            return View();
+        }
+
+        [HttpGet] // Redundante porque toda accion ya es un get
+        public ActionResult Persona() //Para el get, no tiene parametros
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken] //Como que controla que me mande el cliente lo que yo le pido, y en la vista tamb va algo
+        public ActionResult Persona(Persona model) //Yame llega el objeto armado con todas las propiedades adentro
+        {
+            return View(model);
+        }
+
+    }
+}
---
+++ MusicStorePorMi/MusicStorePorMi/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStorePorMi.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
